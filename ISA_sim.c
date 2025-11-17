6bb36d73eba3f8cf7835f650374c485b21ad1d21
shikoubox <shikoubox@github.com>
2025-11-17 13:32:51 +0100

    ISA_sim.c

--- ISA_sim.c
+++ ISA_sim.c
@@ -9,6 +9,7 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 
 #define PROGRAM_SIZE 3
 #define REG_SIZE 4
@@ -22,22 +23,57 @@ int progr[PROGRAM_SIZE] = {
     0x002081b3  // add x3 x1 x2
 };
 
-void r_type(uint32_t instr) {
 
-    int opcode = instr & 0x7F;
-    print(opcode);
-    //int rd = uint32[11:7];
-    //int funct3 = uint32[14:12];
-    //int rs1 = uint32[19:15];
-    //int rs2 = uint32[24:20];
-    //int funct7 = uint32[31:25];
+void print_binary(uint32_t value) {
+    // Array to hold the binary representation
+    char binary_str[33]; // 32 bits + null terminator
+    binary_str[32] = '\0'; // Null-terminate the string
 
-    printf("Value: %" PRIu32 "\n", value);
+    for (int i = 31; i >= 0; i--) {
+        // Set the corresponding bit in the string
+        binary_str[i] = (value & 1) ? '1' : '0';
+        value >>= 1; // Shift the value right by 1 bit
+    }
+
+    printf("The value in binary is: %s\n", binary_str);
+}
+
+void execute_r_type(uint32_t instr) {
+    uint32_t opcode = instr & 0x7F;
+    printf("The value is: %x \n", opcode);
+    print_binary(opcode);
+    uint32_t rd     = (instr >> 7) & 0x1F;
+    uint32_t funct3 = (instr >> 12) & 0x7;
+    uint32_t rs1    = (instr >> 15) & 0x1F;
+    uint32_t rs2    = (instr >> 20) & 0x1F;
+    uint32_t funct7 = (instr >> 25);
+
+    switch (funct3) {
+        case 0x0:
+        switch(funct7) {
+            case 0x0:
+                add(rd, rs1, rs2);
+                break;
+            }
+        break;
+    }
+}
+
+
+void add(uint32_t rd, uint32_t rs1, uint32_t rs2){
+    printf("rd: ");
+    print_binary(rd);
+    printf("\n rs1:");
+    print_binary(rs1);
+    printf("\nrs2:");
+    print_binary(rs2);
 }
 
 
 int main() {
     printf("Hello RISC-V World!\n");
+
+    execute_r_type(0x00FF00F3);
 
     pc = 0;
 
