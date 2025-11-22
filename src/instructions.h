84298b3a0a4cfb91f7ff911654ab7aa6b00e8d69
GWINNERO <Emiloest@gmail.com>
2025-11-22 16:38:37 +0100

    get_pc set_pc

--- src/instructions.h
+++ src/instructions.h
@@ -12,6 +12,8 @@ void add(uint32_t rd, uint32_t rs1, uint32_t rs2);
 void execute_r_type(uint32_t instr);
 void set_register(uint32_t reg, uint32_t value);
 uint32_t get_register(uint32_t reg);
+void set_pc(uint32_t value);
+uint32_t get_pc();
 
 //void get_bits(uint32 x, int hi, int lo);
 
